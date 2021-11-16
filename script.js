33a244c77bad321185df026904be42106c0b5c46
Julian Sehne <julian.sehne@denkbares.com>
2021-11-16 09:14:33 +0100

    Created Functions: getRandom number fromInc, toincl checkPairAgainstRestrictions moveOver (get element from array, remove it and push it to different array) Creating Secret Santa loop

--- script.js
+++ script.js
@@ -60,3 +60,53 @@ var setErrorMessage = (message) => {
   errorMessage.textContent = message;
 }
 
+calculateButton.onclick = () => {
+  let namesKeys = Array.from( namesMap.keys() );
+  const length = namesKeys.length;
+
+  let currentPair = [];
+  moveOver(namesKeys, currentPair, getRandom(0, namesKeys.length - 1));
+  let completeOrder = [];
+  
+
+  while (completeOrder.length != length) {
+    moveOver(namesKeys, currentPair, getRandom(0, namesKeys.length - 1));
+    if(!checkPairAgainstRestriction(currentPair)){
+      moveOver(currentPair, namesKeys, 1);
+      while (completeOrder.length > 0) {
+        moveOver(completeOrder, namesKeys, 0);        
+      }
+      console.log(restrictionArray, currentPair);
+      continue;      
+    }
+    moveOver(currentPair, completeOrder, 0);
+    console.log(completeOrder, completeOrder.length, length);
+  }
+
+
+}
+
+var moveOver = (fromArray, ToArray, index) => {
+  ToArray.push(fromArray[index]);
+  fromArray.splice(fromArray.indexOf(fromArray[index]),1);
+}
+
+var getRandom = (fromIncl, toIncl) => {
+  let random = 0;
+  random = parseInt((Math.random() * (toIncl - fromIncl + 1) ) + fromIncl);
+            
+  return random;
+}
+
+var checkPairAgainstRestriction = (pair) => {
+  
+  for (let currentRestriction = 0; currentRestriction < restrictionIntegerArray.length; currentRestriction++) {
+    for (let second = 1; second < restrictionIntegerArray[currentRestriction].length; second++) {
+      let first = second - 1;  
+      if(first == pair[0] && second == pair[1]){
+        return false;
+      }
+    }    
+  }
+  return true;
+}
